52dcc17bae28679e773faa1cb0df1393bdc3d3b6
Alexandur06 <a.velev.tues@gmail.com>
2022-04-06 14:56:49 +0300

    kavichki

---
+++ asd.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 6a6ob on 4/6/2022.
+//
+
+#include "asd.h"
---
+++ asd.h
@@ -0,0 +1,16 @@
+//
+// Created by 6a6ob on 4/6/2022.
+//
+
+#ifndef KRONOS_ASD_H
+#define KRONOS_ASD_H
+
+
+
+class asd {
+
+};
+
+
+
+#endif //KRONOS_ASD_H
